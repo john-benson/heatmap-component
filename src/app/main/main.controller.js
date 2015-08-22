72329099f866465df3687faf8b433f953b80919a
John Benson <johnedwardbenson@gmail.com>
2015-08-22 18:32:10 -0300

    Working on selecting and removing categories and agencies.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,7 +9,7 @@
   function MainController($scope, moment, boundries, agencyServiceRequests, _, $timeout, $mdSidenav, $mdUtil, heatmapConfiguration) {
     var vm = this;
 
-    var defaultSparklineChartOptions = {
+    var sparklineOptions = {
       chart: {
         type: 'sparklinePlus',
         showLastValue: false,
@@ -31,14 +31,24 @@
       }
     };
 
-    // TODO Change this be dynamic
-    var serviceRequests = agencyServiceRequests['License & Inspections']['Maintenance Residential'];
+    var subSparklineOptions = _.cloneDeep(sparklineOptions);
+    subSparklineOptions.chart.width = 220;
+    subSparklineOptions.chart.margin = {
+      top: 15,
+      left: 10,
+      right: 0,
+      bottom: 0
+    };
+
+    vm.selectedAgency = 'License & Inspections';
+    vm.selectedCategory = 'Maintenance Residential';
+    vm.selectedStatus = 'Open';
+    vm.selectedStartDate = moment(1435719600000);
+    vm.selectedEndDate = moment(1438311600000);
 
     $scope.moment = moment;
 
     this.markerControls = {};
-    this.startDate = moment(1435719600000);
-    this.endDate = moment(1438311600000);
     this.sparklineAgencyCharts = {};
 
     this.map = {
@@ -75,38 +85,53 @@
       return timeseries;
     };
 
-    this.buildTimeSeriesData = function () {
+    this.buildTimeSeriesData = function (requestData, filterParams, startDate, endDate) {
 
-      for (var agencyName in agencyServiceRequests) {
+      if (filterParams instanceof Date) {
+        startDate = filterParams;
+        endDate = startDate;
+        filterParams = null;
+      }
 
-        this.sparklineAgencyCharts[agencyName] = {
-          chartOptions: defaultSparklineChartOptions,
+      var charts = {};
+
+      for (var agencyName in requestData) {
+
+        charts[agencyName] = {
+          chartOptions: sparklineOptions,
           chartData : { },
           subCharts: { },
+          showSubCharts: false,
           totalCount: 0
         };
 
-        for (var category in agencyServiceRequests[agencyName]) {
+        for (var category in requestData[agencyName]) {
 
-          var timeseriesData = vm.fillEmptyDays(_.countBy(agencyServiceRequests[agencyName][category], function(item) {
-            return item.request.requested_day;
-          }), this.startDate, this.endDate);
+          var categoryServiceRequests = _.pluck(requestData[agencyName][category], 'request');
+
+          if (filterParams) {
+            categoryServiceRequests = _.filter(categoryServiceRequests, filterParams);
+          }
+
+          var timeseriesData = vm.fillEmptyDays(_.countBy(categoryServiceRequests, function(item) {
+            return item.requested_day;
+          }), startDate, endDate);
 
           for ( var timestamp in timeseriesData ){
             var count = timeseriesData[timestamp];
 
-            if(!this.sparklineAgencyCharts[agencyName].chartData[timestamp]) {
-              this.sparklineAgencyCharts[agencyName].chartData[timestamp] = 0;
+            if(!charts[agencyName].chartData[timestamp]) {
+              charts[agencyName].chartData[timestamp] = 0;
             }
 
-            this.sparklineAgencyCharts[agencyName].chartData[timestamp] += count;
-            this.sparklineAgencyCharts[agencyName].totalCount += count;
+            charts[agencyName].chartData[timestamp] += count;
+            charts[agencyName].totalCount += count;
           }
 
           var items = this.convertAndSortTimeseriesData(timeseriesData);
 
-          this.sparklineAgencyCharts[agencyName].subCharts[category] = {
-            chartOptions : defaultSparklineChartOptions,
+          charts[agencyName].subCharts[category] = {
+            chartOptions : subSparklineOptions,
             chartData: items,
             totalCount: _.reduce(items, function (total, item, iv) {
               return total + item.y
@@ -114,9 +139,10 @@
           }
         }
 
-        this.sparklineAgencyCharts[agencyName].chartData = this.convertAndSortTimeseriesData(this.sparklineAgencyCharts[agencyName].chartData);
-
+        charts[agencyName].chartData = this.convertAndSortTimeseriesData(charts[agencyName].chartData);
       }
+
+      return charts;
     };
 
     this.convertAndSortTimeseriesData = function (data) {
@@ -138,8 +164,8 @@
         var layerData = [],
             modFactor = 16 - vm.map.zoom;
 
-        for (var i = 0; i < serviceRequests.length; i++ ) {
-          var item = serviceRequests[i];
+        for (var i = 0; i < vm.serviceRequests.length; i++ ) {
+          var item = vm.serviceRequests[i];
 
           if (gMap.getBounds().contains(new google.maps.LatLng(item.coords.latitude, item.coords.longitude)) &&
               vm.map.zoom > 9 &&
@@ -155,15 +181,41 @@
     this.showMarkers = function (gMap) {
       vm.map.markers = [];
 
-      for (var i = 0; i < serviceRequests.length; i++ ) {
-        var item = serviceRequests[i];
+      for (var i = 0; i < vm.serviceRequests.length; i++ ) {
+        var item = vm.serviceRequests[i];
         if (gMap.getBounds().contains(new google.maps.LatLng(item.coords.latitude, item.coords.longitude)) && vm.map.zoom > 16) {
           vm.map.markers.push(item);
         }
       }
     };
 
-    this.buildTimeSeriesData();
+    this.addAgency = function (agency) {
+      vm.selectedAgencies.push(agency);
+    };
+
+    this.removeAgency = function (agency) {
+      vm.selectedAgencies = _.remove(vm.selectedAgencies, function (item)item {
+        return item === agency;
+      });
+    };
+
+    this.addCategory = function (category) {
+      vm.selectedCategories.push(category);
+    };
+
+    this.removeCategory = function (category) {
+      vm.selectedCategories = _.remove(vm.selectedCategories, function (item)item {
+        return item === category;
+      });
+    };
+
+    this.showAgencyCategories = function (agency) {
+      agency.showSubCharts = !agency.showSubCharts;
+    };
+
+    vm.serviceRequests = agencyServiceRequests[vm.selectedAgency][vm.selectedCategory];
+
+    vm.sparklineAgencyCharts = vm.buildTimeSeriesData(agencyServiceRequests, {status : vm.selectedStatus}, vm.selectedStartDate, vm.selectedEndDate);
 
   }
 })();
